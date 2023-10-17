b2afe703314db4519b19b1ce05db1e1e28a34b63
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-10-16 18:41:13 -0600

    add footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,21 @@
-import Image from "next/image"
-import Link from "next/link"
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FOOTER_LINKS } from "@/constants";
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
 
 const Footer = () => {
   return (
@@ -7,22 +23,26 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image 
-            src="hilink-logo.svg"
-            alt="logo"
-            width={74}
-            height={29}
-            />
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
-            
+            {FOOTER_LINKS.map((column) => (
+              <FooterColumn title={column.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {
+                    column.links.map((link) => (
+                      <Link href="/" key={link}>{link}</Link>
+                    ))
+                  }
+                </ul>
+              </FooterColumn>
+            ))}
           </div>
         </div>
-
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
